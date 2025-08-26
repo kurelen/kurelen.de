843fb487962e730af24f7d972b92153362334756
Alexander Jandt <kurelen@pm.me>
2025-08-26 12:39:29 +0200

    Add @next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {};
 
-export default withNextIntl(nextConfig);
+const withNextIntl = createNextIntlPlugin();
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+export default withBundleAnalyzer(withNextIntl(nextConfig));
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,14 @@ export const metadata: Metadata = {
   title: "kurelen.de",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen">
-          {children}
-      </body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
